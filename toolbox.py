fd23ac8cdd168177dd568fec48893f38e8db1b31
Haoyin Xu <haoyinxu@gmail.com>
2021-02-03 21:29:18 -0500

    Create function file and notebook stubs

---
+++ toolbox.py
@@ -0,0 +1,8 @@
+"""
+Coauthors: Michael Ainsworth
+           Madi Kusmanov
+           Yu-Chung Peng
+           Haoyin Xu
+"""
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
