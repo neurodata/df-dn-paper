2f960f852f1084c9c00463d3dc14a5ee272c4058
Haoyin Xu <haoyinxu@gmail.com>
2021-02-08 11:13:02 -0500

    Add multiclass function for rf
    Co-Authored-By: ypeng22 <68977380+ypeng22@users.noreply.github.com>

--- toolbox.py
+++ toolbox.py
@@ -5,4 +5,52 @@ Coauthors: Michael Ainsworth
            Haoyin Xu
 """
 import numpy as np
+from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
+
+
+def run_rf_image(
+    model,
+    train_images,
+    train_labels,
+    test_images,
+    test_labels,
+    fraction_of_train_samples,
+    classes,
+):
+    """
+    Peforms multiclass predictions for a random forest classifier
+    """
+    num_train_samples = []
+    for cls in classes:
+        num_train_samples.append(
+            int(np.sum(train_labels == cls) * fraction_of_train_samples)
+        )
+
+    # Obtain only train images and labels for selected classes
+    image_ls = []
+    label_ls = []
+    for cls in classes:
+        image_ls.append(train_images[train_labels == cls][: num_train_samples[cls]])
+        label_ls.append(np.repeat(cls, num_train_samples[cls]))
+
+    train_images = np.concatenate(image_ls)
+    train_labels = np.concatenate(label_ls)
+
+    # Obtain only test images and labels for selected classes
+    image_ls = []
+    label_ls = []
+    for cls in classes:
+        image_ls.append(test_images[test_labels == cls])
+        label_ls.append(np.repeat(cls, np.sum(test_labels[cls])))
+
+    test_images = np.concatenate(image_ls)
+    test_labels = np.concatenate(label_ls)
+
+    # Train the model
+    model.fit(train_images, train_labels)
+
+    # Test the model
+    test_preds = model.predict(test_images)
+
+    return accuracy_score(test_labels, test_preds)
