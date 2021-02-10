b46c835ecee5bd8b716d29ece93aeb8307128580
Haoyin Xu <haoyinxu@gmail.com>
2021-02-09 19:49:43 -0500

    Add multiclass function for dn

--- toolbox.py
+++ toolbox.py
@@ -50,3 +50,83 @@ def run_rf_image(
     test_preds = model.predict(test_images)
 
     return accuracy_score(test_labels, test_preds)
+
+
+def run_dn_image(
+    model,
+    trainset,
+    train_labels,
+    testset,
+    test_labels,
+    fraction_of_train_samples,
+    classes,
+    epochs=5,
+    lr=0.001,
+    batch=16,
+):
+    """
+    Peforms multiclass predictions for a deep network classifier
+    """
+    class_idxs = []
+    for cls in classes:
+        class_idx = np.argwhere(train_labels == cls).flatten()
+        np.random.shuffle(class_idx)
+        class_idx = class_idx[: int(len(class_idx) * fraction_of_train_samples)]
+        class_idxs.append(class_idx)
+
+    np.random.shuffle(class_idxs)
+
+    train_idxs = np.concatenate(class_idxs)
+    train_sampler = torch.utils.data.sampler.SubsetRandomSampler(train_idxs)
+    train_loader = torch.utils.data.DataLoader(
+        trainset, batch_size=batch, num_workers=4, sampler=train_sampler
+    )
+
+    class_idxs = []
+    for cls in classes:
+        class_idx = np.argwhere(test_labels == cls).flatten()
+        class_idxs.append(class_idx)
+
+    test_idxs = np.concatenate(class_idxs)
+    test_sampler = torch.utils.data.sampler.SubsetRandomSampler(test_idxs)
+    test_loader = torch.utils.data.DataLoader(
+        testset, batch_size=batch, shuffle=False, num_workers=4, sampler=test_sampler
+    )
+
+    # define model
+    dev = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model.to(dev)
+    # loss and optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
+
+    for epoch in range(epochs):  # loop over the dataset multiple times
+
+        for i, data in enumerate(train_loader, 0):
+            # get the inputs
+            inputs, labels = data
+            inputs = inputs.clone().detach().to(dev)
+            labels = labels.clone().detach().to(dev)
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+    # test the model
+    correct = torch.tensor(0).to(dev)
+    total = torch.tensor(0).to(dev)
+    with torch.no_grad():
+        for data in test_loader:
+            images, labels = data
+            labels = labels.clone().detach().to(dev)
+            images = images.clone().detach().to(dev)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels.view(-1)).sum().item()
+    accuracy = float(correct) / float(total)
+    return accuracy
