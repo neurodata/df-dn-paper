4156851d9ed702cee1302b287802132495159574
Michael Ainsworth <michael.j.ainsworth@gmail.com>
2021-05-03 18:33:53 -0400

    Include file for generating model wall times

--- cc18kappaece.py
+++ cc18kappaece.py
@@ -1,97 +1,82 @@
 import numpy as np
-from keras.utils import to_categorical
-
 import matplotlib.pyplot as plt
-import matplotlib as mpl
-mpl.rcParams['lines.linewidth'] = 6
-
-import seaborn as sns
-import pandas as pd
-from random import choices
 from random import sample
-
 from tqdm.notebook import tqdm
-
-from scipy import stats
-
 from sklearn import datasets
 from sklearn.preprocessing import StandardScaler
-
 from sklearn.neural_network import MLPClassifier
-from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import RandomizedSearchCV
-from sklearn.model_selection import PredefinedSplit
-
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import confusion_matrix
-
-from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold, StratifiedKFold
-
 from sklearn.metrics import cohen_kappa_score
 import ast
-
 import openml
 
 
-
-X_data_list = []
-y_data_list = []
-dataset_name = []
-
-for task_num, task_id in enumerate(tqdm(openml.study.get_suite("OpenML-CC18").tasks)):
-    try:
-        successfully_loaded = True
-        dataset = openml.datasets.get_dataset(openml.tasks.get_task(task_id).dataset_id)
-        print(dataset)
-        dataset_name.append(dataset.name)
-        X, y, is_categorical, _ = dataset.get_data(
-            dataset_format="array", target=dataset.default_target_attribute
-        )
-        _, y = np.unique(y, return_inverse = True)
-        #X = np.nan_to_num(X[:, np.where(np.array(is_categorical) == False)[0]])
-        X = np.nan_to_num(X)
-    except TypeError:
-        print("Skipping Dataset {}".format(dataset_idx))
-        print()
-        successfully_loaded = False
-    if successfully_loaded and np.shape(X)[1] > 0:
-        print('\n\nSuccess: ', task_num)
-        X_data_list.append(X)
-        y_data_list.append(y)
-     
-    
-sam = []
-feat = []
-for i in X_data_list:
-    sam.append(i.shape[0])
-    feat.append(i.shape[1])
-    print('Samples: ', i.shape[0])
-    print('Features: ', i.shape[1])
-    print('\n')
-
-    
-unique_classes = []
-for i in y_data_list:
-    unique_classes.append(len(np.unique(i)))
-print(unique_classes)
-
-count = 0
-for i in unique_classes:
-    if i == 2:
-        count += 1
-print(count)
-
-
-
-def load_result(filename):
+def load_cc18():
     """
-    Loads results from specified file
+    Import datasets from OpenML-CC18 dataset suite
     """
-    return np.loadtxt(filename)
+    X_data_list = []
+    y_data_list = []
+    dataset_name = []
+
+    for task_num, task_id in enumerate(tqdm(openml.study.get_suite("OpenML-CC18").tasks)):
+        try:
+            successfully_loaded = True
+            dataset = openml.datasets.get_dataset(openml.tasks.get_task(task_id).dataset_id)
+            print(dataset)
+            dataset_name.append(dataset.name)
+            X, y, is_categorical, _ = dataset.get_data(
+                dataset_format="array", target=dataset.default_target_attribute
+            )
+            _, y = np.unique(y, return_inverse = True)
+            #X = np.nan_to_num(X[:, np.where(np.array(is_categorical) == False)[0]])
+            X = np.nan_to_num(X)
+        except TypeError:
+            print("Skipping Dataset {}".format(dataset_idx))
+            print()
+            successfully_loaded = False
+        if successfully_loaded and np.shape(X)[1] > 0:
+            print('\n\nSuccess: ', task_num)
+            X_data_list.append(X)
+            y_data_list.append(y)
+
+    return X_data_list, y_data_list, dataset_name
+    
+# sam = []
+# feat = []
+# for i in X_data_list:
+#     sam.append(i.shape[0])
+#     feat.append(i.shape[1])
+#     print('Samples: ', i.shape[0])
+#     print('Features: ', i.shape[1])
+#     print('\n')
+
+    
+# unique_classes = []
+# for i in y_data_list:
+#     unique_classes.append(len(np.unique(i)))
+# print(unique_classes)
+
+# count = 0
+# for i in unique_classes:
+#     if i == 2:
+#         count += 1
+# print(count)
+
+
+
+# def load_result(filename):
+#     """
+#     Loads results from specified file
+#     """
+#     return np.loadtxt(filename)
 
 def read_params_txt(filename):
+    """
+    Read and parse optimized parameters from text file
+    """
     params = []
     f = open(filename, 'r').read()
     f = f.split('\n')
@@ -102,7 +87,9 @@ def read_params_txt(filename):
     return params
 
 def random_sample_new(data, training_sample_sizes):
-    
+    """
+    Given X_data and a list of training sample size, randomly sample indices to be used. Larger sample sizes include all indices from smaller sample sizes.
+    """
     temp_inds = []
 
     ordered = [i for i in range(len(data))]
@@ -124,6 +111,9 @@ def random_sample_new(data, training_sample_sizes):
     return final_inds
 
 def get_ece(predicted_posterior, predicted_label, true_label, num_bins=40):
+    """
+    Return ece score
+    """
     poba_hist = []
     accuracy_hist = []
     bin_size = 1/num_bins
@@ -153,31 +143,40 @@ def get_ece(predicted_posterior, predicted_label, true_label, num_bins=40):
     score /= total_sample
     return score
 
+def sample_large_datasets(X_data, y_data):
+    """
+    For large datasets with over 10000 samples, resample the data to only include 10000 random samples.
+    """
+    inds = [i for i in range(X_data.shape[0])]
+    fin = sorted(sample(inds, 10000))
+    return X_data[fin], y_data[fin]
 
+X_data_list, y_data_list, dataset_name = load_cc18()
 
 all_params = read_params_txt("all_parameters_final.txt")
 
-
-small_datasets = [i for i,value in enumerate(sam) if value < 10000]
-
+train_indices = [i for i in range(72)]
 
 
-all_sample_sizes = np.zeros((len(small_datasets), 8))
+all_sample_sizes = np.zeros((len(train_indices), 8))
 
 reps = 5
 
-rf_evolution = np.zeros((8*len(small_datasets),5))
-dn_evolution = np.zeros((8*len(small_datasets),5))
+rf_evolution = np.zeros((8*len(train_indices),5))
+dn_evolution = np.zeros((8*len(train_indices),5))
 
-rf_evolution_ece = np.zeros((8*len(small_datasets),5))
-dn_evolution_ece = np.zeros((8*len(small_datasets),5))
+rf_evolution_ece = np.zeros((8*len(train_indices),5))
+dn_evolution_ece = np.zeros((8*len(train_indices),5))
 
-for dataset_index, dataset in enumerate(small_datasets):
+for dataset_index, dataset in enumerate(train_indices):
     
     print('\n\n\n\nDATASET: ', dataset)
 
     X = X_data_list[dataset]
     y = y_data_list[dataset]
+   
+    if X.shape[0] > 10000:
+        X, y = sample_large_datasets(X,y)
     
     scaler = StandardScaler()
     scaler.fit(X)
@@ -215,7 +214,7 @@ for dataset_index, dataset in enumerate(small_datasets):
             
             for ii in range(reps):
 #                 print('Repetition: ', ii)
-                rf = RandomForestClassifier(**all_params[dataset][1], n_estimators=500)
+                rf = RandomForestClassifier(**all_params[dataset][1], n_estimators=500, n_jobs=-1)
                 mlp = MLPClassifier(**all_params[dataset][0])
 
                 rf.fit(X_train_new, y_train_new)
@@ -256,9 +255,9 @@ print(rf_evolution_ece)
 
 
 
-np.savetxt('sample_sizes_ecekappa_small.txt', all_sample_sizes)     
+np.savetxt('sample_sizes_ecekappa_largest.txt', all_sample_sizes)     
 
-np.savetxt('dn_evolution_kappa_small.txt', dn_evolution)     
-np.savetxt('rf_evolution_kappa_small.txt', rf_evolution)   
-np.savetxt('dn_evolution_ece_small.txt', dn_evolution_ece)     
-np.savetxt('rf_evolution_ece_small.txt', rf_evolution_ece)   
+np.savetxt('dn_evolution_kappa_largest.txt', dn_evolution)     
+np.savetxt('rf_evolution_kappa_largest.txt', rf_evolution)   
+np.savetxt('dn_evolution_ece_largest.txt', dn_evolution_ece)     
+np.savetxt('rf_evolution_ece_largest.txt', rf_evolution_ece)   
---
+++ cc18times.py
@@ -0,0 +1,201 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from random import sample
+from tqdm.notebook import tqdm
+from sklearn import datasets
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import KFold, StratifiedKFold
+import ast
+import openml
+import timeit
+
+
+def load_cc18():
+    """
+    Import datasets from OpenML-CC18 dataset suite
+    """
+    X_data_list = []
+    y_data_list = []
+    dataset_name = []
+
+    for task_num, task_id in enumerate(tqdm(openml.study.get_suite("OpenML-CC18").tasks)):
+        try:
+            successfully_loaded = True
+            dataset = openml.datasets.get_dataset(openml.tasks.get_task(task_id).dataset_id)
+            dataset_name.append(dataset.name)
+            X, y, is_categorical, _ = dataset.get_data(
+                dataset_format="array", target=dataset.default_target_attribute
+            )
+            _, y = np.unique(y, return_inverse = True)
+            X = np.nan_to_num(X)
+        except TypeError:
+            successfully_loaded = False
+        if successfully_loaded and np.shape(X)[1] > 0:
+            X_data_list.append(X)
+            y_data_list.append(y)
+
+    return X_data_list, y_data_list, dataset_name
+
+
+def read_params_txt(filename):
+    """
+    Read and parse optimized parameters from text file
+    """
+    params = []
+    f = open(filename, 'r').read()
+    f = f.split('\n')
+    f = f[:-1]
+    for ind, i in enumerate(f):
+        temp = ast.literal_eval(f[ind])
+        params.append(temp)
+    return params
+
+
+def random_sample_new(data, training_sample_sizes):
+    """
+    Given X_data and a list of training sample size, randomly sample indices to be used. Larger sample sizes include all indices from smaller sample sizes.
+    """
+    temp_inds = []
+
+    ordered = [i for i in range(len(data))]
+    minus = 0
+    for ss in range(len(training_sample_sizes)):
+        x = sorted(sample(ordered,training_sample_sizes[ss] - minus))
+        minus += len(x)
+        temp_inds.append(x)
+        ordered = list(set(ordered) - set(x))
+
+    final_inds = []
+    temp = []
+
+    for i in range(len(temp_inds)):
+        cur = temp_inds[i]
+        final_inds.append(sorted(cur + temp))
+        temp = sorted(cur + temp)
+    
+    return final_inds
+
+
+def sample_large_datasets(X_data, y_data):
+    """
+    For large datasets with over 10000 samples, resample the data to only include 10000 random samples.
+    """
+    inds = [i for i in range(X_data.shape[0])]
+    fin = sorted(sample(inds, 10000))
+    return X_data[fin], y_data[fin]
+
+
+def time_rf_cc18(SETUP_CODE, X_train_new, y_train_new, X_test, all_params, dataset):
+    """
+    Use timeit function to measure training and fitting time of the Random Forest classifier using optimized parameters for given dataset.
+    """
+    TEST_CODE = """
+
+rf = RandomForestClassifier(**{}[{}][1], n_estimators=500, n_jobs=-1)
+rf.fit(X_train_new, y_train_new)
+y_pred_rf = rf.predict(X_test)
+
+    """.format(all_params,dataset)
+    time_rf = timeit.repeat(setup=SETUP_CODE_RF, stmt=TEST_CODE, repeat=1, number=1, globals={'X_train_new':X_train_new,
+                                                                                        'y_train_new':y_train_new,
+                                                                                        'X_test':X_test}
+                        )
+    
+    return time_rf
+
+SETUP_CODE_RF = """
+
+from sklearn.ensemble import RandomForestClassifier
+
+"""
+
+
+def time_mlp_cc18(SETUP_CODE, X_train_new, y_train_new, X_test, all_params, dataset):
+    """
+    Use timeit function to measure training and fitting time of the Random Forest classifier using optimized parameters for given dataset.
+    """
+    TEST_CODE = """
+
+mlp = MLPClassifier(**{}[{}][0])
+mlp.fit(X_train_new, y_train_new)
+y_pred_mlp = mlp.predict(X_test)
+
+    """.format(all_params,dataset)
+    time_mlp = timeit.repeat(setup=SETUP_CODE_MLP, stmt=TEST_CODE, repeat=1, number=1, globals={'X_train_new':X_train_new,
+                                                                                        'y_train_new':y_train_new,
+                                                                                        'X_test':X_test}
+                        )
+    
+    return time_mlp
+
+SETUP_CODE_MLP = """
+
+from sklearn.neural_network import MLPClassifier
+
+"""
+
+
+# Load data from CC18 data set suite
+X_data_list, y_data_list, dataset_name = load_cc18()
+dataset_indices = [i for i in range(72)]
+all_params = read_params_txt("all_parameters_final.txt")
+
+# Empty arrays to index times into
+rf_times = np.zeros((8*len(dataset_indices),5))
+dn_times = np.zeros((8*len(dataset_indices),5))
+
+# For each dataset
+for dataset_index, dataset in enumerate(dataset_indices):
+    
+    print('\n\nCurrent Dataset: ', dataset)
+
+    X = X_data_list[dataset]
+    y = y_data_list[dataset]
+    
+    if X.shape[0] > 10000:
+        X, y = sample_large_datasets(X,y)
+
+    scaler = StandardScaler()
+    scaler.fit(X)
+    X = scaler.transform(X)
+        
+    kf = StratifiedKFold(n_splits=5, shuffle=True)
+
+    k_index=0
+    for train_index, test_index in kf.split(X,y):
+        print('CV Fold: ', k_index)
+
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]  
+    
+        temp = np.log10((len(np.unique(y))) * 5)
+        t = (np.log10(X_train.shape[0]) - temp) / 7
+        training_sample_sizes = []
+        for i in range(8):
+            training_sample_sizes.append(round(np.power(10,temp + i*t)))
+    
+        ss_inds = random_sample_new(X_train, training_sample_sizes)
+        
+        for sample_size_index, max_sample_size in enumerate(training_sample_sizes):
+            
+            X_train_new = X_train[ss_inds[sample_size_index]]
+            y_train_new = y_train[ss_inds[sample_size_index]]
+
+            rf_time_temp = time_rf_cc18(SETUP_CODE_RF, X_train_new, y_train_new, X_test, all_params, dataset)
+            mlp_time_temp = time_mlp_cc18(SETUP_CODE_MLP, X_train_new, y_train_new, X_test, all_params, dataset)
+
+            
+            rf_times[sample_size_index + 8*dataset_index][k_index] = rf_time_temp[0]
+            dn_times[sample_size_index + 8*dataset_index][k_index] = mlp_time_temp[0]
+            
+            
+        k_index += 1
+    
+print(rf_times)
+print(dn_times)
+
+# np.savetxt('rf_evolution_times.txt', rf_times)
+# np.savetxt('dn_evolution_times.txt', dn_times)     
+
