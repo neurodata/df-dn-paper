FIX add dim parameter to softmax

benchmark_audition/audio_toolbox.py
@@ -313,7 +313,7 @@ def run_dn_image_es(
 
     # test the model
     model.eval()
-    prob_cal = nn.Softmax()
+    prob_cal = nn.Softmax(dim=1)
     start_time = time.perf_counter()
     test_preds = []
     with torch.no_grad():

benchmark_vision/toolbox.py
@@ -275,7 +275,7 @@ def run_dn_image_set(
     # test the model
     model.eval()
     first = True
-    prob_cal = nn.Softmax()
+    prob_cal = nn.Softmax(dim=1)
     start_time = time.perf_counter()
     test_preds = []
     test_labels = []
@@ -376,7 +376,7 @@ def run_dn_image_es(
     # test the model
     model.eval()
     first = True
-    prob_cal = nn.Softmax()
+    prob_cal = nn.Softmax(dim=1)
     start_time = time.perf_counter()
     test_preds = []
     test_labels = []