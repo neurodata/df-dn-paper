a8876fd4a12cfe5e1259231bed10e02645f0f5b2
NOGA8 <nmudrik1@jhmi.edu>
2021-10-03 19:43:24 -0400

    Generalize hyp. file for additional models

--- benchmark_tabular/old_ver_tabular_cc18_hyper.py
+++ benchmark_tabular/old_ver_tabular_cc18_hyper.py
@@ -1,6 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  3 16:17:50 2021
+
+@author: noga mudrik
+"""
+
 """
 Author: Michael Ainsworth
 """
+
+#%% Define executrion variables (to save memory & execution time)
+reload_data = False # indicator of whether to upload the data again
+nodes_combination = [20, 100, 180, 260, 340, 400] # default is [20, 100, 180, 260, 340, 400]
+dataset_indices_max=72 
+max_shape_to_run=10000
+alpha_range_nn=[0.0001, 0.001, 0.01, 0.1]
+#%% function to return to default values
+def return_to_default():
+    nodes_combination = [20, 100, 180, 260, 340, 400]
+    dataset_indices_max = 72 
+    max_shape_to_run = 10000
+    alpha_range_nn = [0.0001, 0.001, 0.01, 0.1]
+    return nodes_combination,dataset_indices_max,max_shape_to_run
+
+#%% function so save cc18_all_parameters file
 
 #%% Imports
 
@@ -13,34 +36,10 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import RandomizedSearchCV
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier
 import openml
 import itertools
-#%% Define executrion variables (to save memory & execution time)
-reload_data = False # indicator of whether to upload the data again
-nodes_combination = [20, 100, 180, 260, 340, 400] # default is [20, 100, 180, 260, 340, 400]
-dataset_indices_max=72 
-max_shape_to_run=10000
-alpha_range_nn=[0.0001, 0.001, 0.01, 0.1]
-subsample=[0.5,0.8,1.0]
 
-
-#models
-models_to_run={'RF':0,'DN':0,'GBDT':1} # Define which models to run
-classifiers={'DN':MLPClassifier(max_iter=200), 'RF':RandomForestClassifier(n_estimators=500), 'GBDT': GradientBoostingClassifier(n_estimators=500)}
-vararginCV={'DN':{'n_jobs':-1,'verbose':1,'cv':None},
-            'RF':{'n_jobs':-1,'verbose':1,'cv':None},
-            'GBDT':{'n_jobs':None,'verbose':1,'cv':None}}
-#%% function to return to default values
-def return_to_default():
-    nodes_combination = [20, 100, 180, 260, 340, 400]
-    dataset_indices_max = 72 
-    max_shape_to_run = 10000
-    alpha_range_nn = [0.0001, 0.001, 0.01, 0.1]
-    models_to_run={'RF':1,'DN':1,'GBDT':1}
-    return nodes_combination,dataset_indices_max,max_shape_to_run
-
-#%% function so save cc18_all_parameters file
 
 def load_cc18():
     """
@@ -92,9 +91,8 @@ if (reload_data or 'task_id' not in locals()): # Load the data only if required 
     X_data_list, y_data_list, dataset_name = load_cc18()
 
 
-# Empty dict to record optimal parameters
-all_parameters = {model_name:None for model_name,val in models_to_run if val==1}
-best_parameters = {model_name:None for model_name,val in models_to_run if val==1}
+# Empty list to record optimal parameters
+all_parameters = []
 
 
 # Choose dataset indices
@@ -110,31 +108,6 @@ two_layer = list(itertools.combinations(test_list, 2))
 three_layer = list(itertools.combinations(test_list, 3))
 
 node_range = test_list + two_layer + three_layer
-
-# Functions to calculate model performance and parameters.
-
-def create_parameters(model_name,varargin):
-    if model_name=='DN':
-        parameters={"hidden_layer_sizes": varargin['node_range'], "alpha": varargin['alpha_range_nn']}
-    elif model_name=='RF':
-        parameters={"max_features": list(set([round(p / 4), round(np.sqrt(p)), round(p / 3), round(p / 1.5), round(p)]))}
-    elif model_name=='GBDT':
-        parameters={'learning_rate':varargin['alpha_range_nn'],'subsample':varargin['subsample']}
-    else:
-        raise ValueError("Model name is invalid. Please check the keys of models_to_run")
-    return parameters
-        
-def do_calcs_per_model(all_parameters,best_parameters, allparams,model_name,varargin,varCV,classifiers,X,y):
-    model=classifiers[model_name]
-    varCVmodel=varCV[model_name]
-    parameters=create_parameters(model_name,varargin)
-    clf = RandomizedSearchCV(model, parameters, n_jobs=varCVmodel[n_jobs], cv=varCVmodel[cv], verbose=varCVmodel[verbose])
-    clf.fit(X, y)
-    all_parameters[model_name]=parameters
-    best__parameters[model_name]=clf.best_params_
-    allparams[model_name]=clf.cv_results_["params"]
-    return all_parameters, best_parameters,all_params
-
 
 # For each dataset, use randomized hyperparameter search to optimize parameters
 for dataset_index, dataset in enumerate(dataset_indices):
@@ -152,22 +125,32 @@ for dataset_index, dataset in enumerate(dataset_indices):
     scaler = StandardScaler()
     scaler.fit(X)
     X = scaler.transform(X)
-    
+
+    # Deep network hyperparameters
+    parameters = {"hidden_layer_sizes": node_range, "alpha": alpha_range_nn}
+
+    # Random forest hyperparameters
     p = X.shape[1]
-    create_parameters(models_to_run)
-    for model_name,val_run in models_to_run.items():
-        if val==1:
-            if model_name not in classifires:
-                raise ValueError('Model name is not defined in the classifiers dictionary')
-            else:
-                all_parameters, best_parameters,all_params=do_calcs_per_model(all_parameters,
-                                                                              best_parameters, 
-                                                                              allparams,
-                                                                              model_name,
-                                                                              varargin,vararginCV,classifiers,X,y)
+    l = list(
+        set([round(p / 4), round(np.sqrt(p)), round(p / 3), round(p / 1.5), round(p)])
+    )
+    parameters_rf = {"max_features": l}
 
+    mlp = MLPClassifier(max_iter=200)
+    clf = RandomizedSearchCV(mlp, parameters, n_jobs=-1, cv=None, verbose=1)
+    clf.fit(X, y)
 
+    rf = RandomForestClassifier(n_estimators=500)
+    clfrf = RandomizedSearchCV(rf, parameters_rf, n_jobs=-1, verbose=1)
+    clfrf.fit(X, y)
 
+    allparams = clf.cv_results_["params"]
+    allparamsrf = clfrf.cv_results_["params"]
+
+    best_params = clf.best_params_
+    best_paramsrf = clfrf.best_params_
+
+    all_parameters.append([best_params, best_paramsrf])
 
 
 # Save optimal parameters to txt file
