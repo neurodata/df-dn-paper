091806b10eb9b3855164043db935b7ed08bc696b
Haoyin Xu <haoyinxu@gmail.com>
2021-10-18 11:03:16 -0400

    MAINT update names & directories

---
+++ benchmarks/tabular/hist.py
@@ -0,0 +1,183 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 17 15:18:57 2021
+
+@author: noga mudrik
+"""
+
+"""
+Author: Michael Ainsworth
+"""
+
+#%% Imports 
+
+import numpy as np
+import matplotlib.pyplot as plt
+from random import sample
+from tqdm.notebook import tqdm
+from sklearn import datasets
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import StratifiedKFold
+import ast
+import openml
+import time
+import json 
+
+from basic_functions_script import *
+from save_hyperparameters import *
+path_params = 'metrics/dict_parameters'
+with open(path_params+".json",'r') as json_file:
+    dictionary_params = json.load(json_file)
+train_times = {model_name:None for model_name in dictionary_params['classifiers_names']}
+test_times = {model_name:None for model_name in dictionary_params['classifiers_names']}
+shape_2_evolution = dictionary_params['shape_2_evolution'] 
+len_samp_size = dictionary_params['len_samp_size']
+save_times_rewrite={'text_dict':0,'csv':0,'json':0}
+save_methods={'text_dict':1,'csv':1,'json':1}
+#%% Functions
+
+
+def read_params_txt(filename):
+    """
+    Read and parse optimized parameters from text file
+    """
+    params = []
+    f = open(filename, "r").read()
+    f = f.split("\n")
+    f = f[:-1]
+    for ind, i in enumerate(f):
+        temp = ast.literal_eval(f[ind])
+        params.append(temp)
+    return params
+
+
+def random_sample_new(data, training_sample_sizes):
+    """
+    Given X_data and a list of training sample size, randomly sample indices to be used.
+    Larger sample sizes include all indices from smaller sample sizes.
+    """
+    temp_inds = []
+
+    ordered = [i for i in range(len(data))]
+    minus = 0
+    for ss in range(len(training_sample_sizes)):
+        x = sorted(sample(ordered, training_sample_sizes[ss] - minus))
+        minus += len(x)
+        temp_inds.append(x)
+        ordered = list(set(ordered) - set(x))
+
+    final_inds = []
+    temp = []
+
+    for i in range(len(temp_inds)):
+        cur = temp_inds[i]
+        final_inds.append(sorted(cur + temp))
+        temp = sorted(cur + temp)
+
+    return final_inds
+
+
+def sample_large_datasets(X_data, y_data):
+    """
+    For large datasets with over 10000 samples, resample the data to only include
+    10000 random samples.
+    """
+    inds = [i for i in range(X_data.shape[0])]
+    fin = sorted(sample(inds, 10000))
+    return X_data[fin], y_data[fin]
+
+def calculate_time(model,X_train,y_train, X_test):
+    start_time = time.perf_counter()
+    model.fit(X_train, y_train)
+    end_time = time.perf_counter()
+    train_time = end_time - start_time
+    
+    
+    start_time = time.perf_counter()
+    y_pred = model.predict(X_test)
+    end_time = time.perf_counter()
+    test_time = end_time - start_time
+            
+    return train_time,test_time, y_pred
+            
+            
+# Load data from CC18 data set suite
+if (dictionary_params['reload_data'] or 'dataset_name' not in locals()):
+    X_data_list, y_data_list, dataset_name = load_cc18()
+path_best_parameters = 'metrics/cc18_all_parameters_new'
+if 'best_params_dict' not in locals():
+    best_params_dict = open_data(path_best_parameters,'text_dict')
+    
+dataset_indices = [i for i in range(dictionary_params['dataset_indices_max'])]
+
+# Import pretuned hyperparameters
+all_params = read_params_txt("metrics/cc18_all_parameters.txt")
+
+
+# Empty arrays to index times into
+train_test_times = {metric:
+    {model_name:np.zeros((len_samp_size * len(dataset_indices), shape_2_evolution)) for model_name in best_params_dict.keys()} for metric in ['train','test']}
+
+# For each dataset, determine wall times at each sample size
+for dataset_index, dataset in enumerate(dataset_indices):
+
+    print("\n\nCurrent Dataset: ", dataset)
+
+    X = X_data_list[dataset]
+    y = y_data_list[dataset]
+
+    # If data set has over 10000 samples, resample to contain 10000
+    if X.shape[0] > dictionary_params['max_shape_to_run']:
+        X, y = sample_large_datasets(X, y)
+    # Scaling
+    scaler = StandardScaler()
+    scaler.fit(X)
+    X = scaler.transform(X)
+
+    # Implement stratified 5-fold cross validation
+    kf = StratifiedKFold(n_splits=5, shuffle=True)
+    k_index = 0
+    for train_index, test_index in kf.split(X, y):
+
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+
+        # Generate training sample sizes, logorithmically spaced
+        temp = np.log10((len(np.unique(y))) * 5)
+        t = (np.log10(X_train.shape[0]) - temp) / 7
+        training_sample_sizes = []
+        for i in range(len_samp_size):
+            training_sample_sizes.append(round(np.power(10, temp + i * t)))
+
+        ss_inds = random_sample_new(X_train, training_sample_sizes)
+
+        # Iterate through each sample size per dataset
+        for sample_size_index, max_sample_size in enumerate(training_sample_sizes):
+
+            X_train_new = X_train[ss_inds[sample_size_index]]
+            y_train_new = y_train[ss_inds[sample_size_index]]
+            
+            for model_name in best_params_dict.keys():
+                #parameters = create_parameters(model_name,varargin)
+                model = model_define(model_name,best_params_dict,dataset)
+                train_time, test_time, y_pred = calculate_time(model,X_train_new,y_train_new, X_test)
+                train_times[model_name] = train_time
+                test_times[model_name] = test_time
+                if train_test_times['train'][model_name].get(sample_size_index)==None:
+                    train_test_times['train'][model_name]
+                train_test_times['train'][model_name][sample_size_index] + len_samp_size * dataset_index][k_index] = train_time
+                train_test_times['test'][model_name][sample_size_index] + len_samp_size * dataset_index][k_index] = test_time
+
+
+        k_index += 1
+
+
+# Save results as txt files
+save_best_parameters(save_methods =save_methods,save_methods_rewrite = save_times_rewrite ,path_save = "results/times" ,best_parameters= train_test_times )
+        
+#np.savetxt("results/cc18_rf_times_train.txt", rf_times_train)
+#np.savetxt("results/cc18_rf_times_test.txt", rf_times_test)
+#np.savetxt("results/cc18_dn_times_train.txt", dn_times_train)
+#np.savetxt("results/cc18_dn_times_test.txt", dn_times_test)
---
+++ benchmarks/vision/cifar_100.py
@@ -0,0 +1,295 @@
+"""
+Coauthors: Haoyin Xu
+           Yu-Chung Peng
+"""
+from toolbox import *
+
+import argparse
+import random
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+
+import torchvision.models as models
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+
+
+def run_naive_rf():
+    naive_rf_kappa = []
+    naive_rf_ece = []
+    naive_rf_train_time = []
+    naive_rf_test_time = []
+    for classes in classes_space:
+
+        # cohen_kappa vs num training samples (naive_rf)
+        for samples in samples_space:
+            RF = RandomForestClassifier(n_estimators=100, n_jobs=-1)
+            cohen_kappa, ece, train_time, test_time = run_rf_gbdt_image_set(
+                RF,
+                cifar_train_images,
+                cifar_train_labels,
+                cifar_test_images,
+                cifar_test_labels,
+                samples,
+                classes,
+            )
+            naive_rf_kappa.append(cohen_kappa)
+            naive_rf_ece.append(ece)
+            naive_rf_train_time.append(train_time)
+            naive_rf_test_time.append(test_time)
+
+    print("naive_rf finished")
+    write_result(prefix + "naive_rf_kappa.txt", naive_rf_kappa)
+    write_result(prefix + "naive_rf_ece.txt", naive_rf_ece)
+    write_result(prefix + "naive_rf_train_time.txt", naive_rf_train_time)
+    write_result(prefix + "naive_rf_test_time.txt", naive_rf_test_time)
+
+
+def run_cnn32():
+    cnn32_kappa = []
+    cnn32_ece = []
+    cnn32_train_time = []
+    cnn32_test_time = []
+    for classes in classes_space:
+
+        # cohen_kappa vs num training samples (cnn32)
+        for samples in samples_space:
+            # train data
+            cifar_trainset = datasets.CIFAR100(
+                root="./", train=True, download=True, transform=data_transforms
+            )
+            cifar_train_labels = np.array(cifar_trainset.targets)
+
+            # test data
+            cifar_testset = datasets.CIFAR100(
+                root="./", train=False, download=True, transform=data_transforms
+            )
+            cifar_test_labels = np.array(cifar_testset.targets)
+
+            cnn32 = SimpleCNN32Filter(len(classes))
+            train_loader, valid_loader, test_loader = create_loaders_es(
+                cifar_train_labels,
+                cifar_test_labels,
+                classes,
+                cifar_trainset,
+                cifar_testset,
+                samples,
+            )
+            cohen_kappa, ece, train_time, test_time = run_dn_image_es(
+                cnn32,
+                train_loader,
+                valid_loader,
+                test_loader,
+            )
+            cnn32_kappa.append(cohen_kappa)
+            cnn32_ece.append(ece)
+            cnn32_train_time.append(train_time)
+            cnn32_test_time.append(test_time)
+
+    print("cnn32 finished")
+    write_result(prefix + "cnn32_kappa.txt", cnn32_kappa)
+    write_result(prefix + "cnn32_ece.txt", cnn32_ece)
+    write_result(prefix + "cnn32_train_time.txt", cnn32_train_time)
+    write_result(prefix + "cnn32_test_time.txt", cnn32_test_time)
+
+
+def run_cnn32_2l():
+    cnn32_2l_kappa = []
+    cnn32_2l_ece = []
+    cnn32_2l_train_time = []
+    cnn32_2l_test_time = []
+    for classes in classes_space:
+
+        # cohen_kappa vs num training samples (cnn32_2l)
+        for samples in samples_space:
+            # train data
+            cifar_trainset = datasets.CIFAR100(
+                root="./", train=True, download=True, transform=data_transforms
+            )
+            cifar_train_labels = np.array(cifar_trainset.targets)
+
+            # test data
+            cifar_testset = datasets.CIFAR100(
+                root="./", train=False, download=True, transform=data_transforms
+            )
+            cifar_test_labels = np.array(cifar_testset.targets)
+
+            cnn32_2l = SimpleCNN32Filter2Layers(len(classes))
+            train_loader, valid_loader, test_loader = create_loaders_es(
+                cifar_train_labels,
+                cifar_test_labels,
+                classes,
+                cifar_trainset,
+                cifar_testset,
+                samples,
+            )
+            cohen_kappa, ece, train_time, test_time = run_dn_image_es(
+                cnn32_2l,
+                train_loader,
+                valid_loader,
+                test_loader,
+            )
+            cnn32_2l_kappa.append(cohen_kappa)
+            cnn32_2l_ece.append(ece)
+            cnn32_2l_train_time.append(train_time)
+            cnn32_2l_test_time.append(test_time)
+
+    print("cnn32_2l finished")
+    write_result(prefix + "cnn32_2l_kappa.txt", cnn32_2l_kappa)
+    write_result(prefix + "cnn32_2l_ece.txt", cnn32_2l_ece)
+    write_result(prefix + "cnn32_2l_train_time.txt", cnn32_2l_train_time)
+    write_result(prefix + "cnn32_2l_test_time.txt", cnn32_2l_test_time)
+
+
+def run_cnn32_5l():
+    cnn32_5l_kappa = []
+    cnn32_5l_ece = []
+    cnn32_5l_train_time = []
+    cnn32_5l_test_time = []
+    for classes in classes_space:
+
+        # cohen_kappa vs num training samples (cnn32_5l)
+        for samples in samples_space:
+            # train data
+            cifar_trainset = datasets.CIFAR100(
+                root="./", train=True, download=True, transform=data_transforms
+            )
+            cifar_train_labels = np.array(cifar_trainset.targets)
+
+            # test data
+            cifar_testset = datasets.CIFAR100(
+                root="./", train=False, download=True, transform=data_transforms
+            )
+            cifar_test_labels = np.array(cifar_testset.targets)
+
+            cnn32_5l = SimpleCNN32Filter5Layers(len(classes))
+            train_loader, valid_loader, test_loader = create_loaders_es(
+                cifar_train_labels,
+                cifar_test_labels,
+                classes,
+                cifar_trainset,
+                cifar_testset,
+                samples,
+            )
+            cohen_kappa, ece, train_time, test_time = run_dn_image_es(
+                cnn32_5l,
+                train_loader,
+                valid_loader,
+                test_loader,
+            )
+            cnn32_5l_kappa.append(cohen_kappa)
+            cnn32_5l_ece.append(ece)
+            cnn32_5l_train_time.append(train_time)
+            cnn32_5l_test_time.append(test_time)
+
+    print("cnn32_5l finished")
+    write_result(prefix + "cnn32_5l_kappa.txt", cnn32_5l_kappa)
+    write_result(prefix + "cnn32_5l_ece.txt", cnn32_5l_ece)
+    write_result(prefix + "cnn32_5l_train_time.txt", cnn32_5l_train_time)
+    write_result(prefix + "cnn32_5l_test_time.txt", cnn32_5l_test_time)
+
+
+def run_resnet18():
+    resnet18_kappa = []
+    resnet18_ece = []
+    resnet18_train_time = []
+    resnet18_test_time = []
+    for classes in classes_space:
+
+        # cohen_kappa vs num training samples (resnet18)
+        for samples in samples_space:
+            # train data
+            cifar_trainset = datasets.CIFAR100(
+                root="./", train=True, download=True, transform=data_transforms
+            )
+            cifar_train_labels = np.array(cifar_trainset.targets)
+
+            # test data
+            cifar_testset = datasets.CIFAR100(
+                root="./", train=False, download=True, transform=data_transforms
+            )
+            cifar_test_labels = np.array(cifar_testset.targets)
+
+            res = models.resnet18(pretrained=True)
+            num_ftrs = res.fc.in_features
+            res.fc = nn.Linear(num_ftrs, len(classes))
+            train_loader, valid_loader, test_loader = create_loaders_es(
+                cifar_train_labels,
+                cifar_test_labels,
+                classes,
+                cifar_trainset,
+                cifar_testset,
+                samples,
+            )
+            cohen_kappa, ece, train_time, test_time = run_dn_image_es(
+                res,
+                train_loader,
+                valid_loader,
+                test_loader,
+            )
+            resnet18_kappa.append(cohen_kappa)
+            resnet18_ece.append(ece)
+            resnet18_train_time.append(train_time)
+            resnet18_test_time.append(test_time)
+
+    print("resnet18 finished")
+    write_result(prefix + "resnet18_kappa.txt", resnet18_kappa)
+    write_result(prefix + "resnet18_ece.txt", resnet18_ece)
+    write_result(prefix + "resnet18_train_time.txt", resnet18_train_time)
+    write_result(prefix + "resnet18_test_time.txt", resnet18_test_time)
+
+
+if __name__ == "__main__":
+    torch.multiprocessing.freeze_support()
+
+    # Example usage: python cifar_100.py -m 90
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", help="class number")
+    args = parser.parse_args()
+    n_classes = int(args.m)
+    prefix = args.m + "_class/"
+    samples_space = np.geomspace(100, 10000, num=8, dtype=int)
+
+    nums = list(range(100))
+    random.shuffle(nums)
+    classes_space = list(combinations_45(nums, n_classes))
+
+    # normalize
+    scale = np.mean(np.arange(0, 256))
+    normalize = lambda x: (x - scale) / scale
+
+    # train data
+    cifar_trainset = datasets.CIFAR100(
+        root="./", train=True, download=True, transform=None
+    )
+    cifar_train_images = normalize(cifar_trainset.data)
+    cifar_train_labels = np.array(cifar_trainset.targets)
+
+    # test data
+    cifar_testset = datasets.CIFAR100(
+        root="./", train=False, download=True, transform=None
+    )
+    cifar_test_images = normalize(cifar_testset.data)
+    cifar_test_labels = np.array(cifar_testset.targets)
+
+    cifar_train_images = cifar_train_images.reshape(-1, 32 * 32 * 3)
+    cifar_test_images = cifar_test_images.reshape(-1, 32 * 32 * 3)
+
+    run_naive_rf()
+
+    data_transforms = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+    )
+
+    run_cnn32()
+    run_cnn32_2l()
+    run_cnn32_5l()
+
+    data_transforms = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+        ]
+    )
+
+    run_resnet18()
