38675cd604253271cc013f99cf957f497981c3be
Adway Kanhere <adway.kanhere@gmail.com>
2021-12-16 17:11:57 -0500

    removed plot_waveform function from FSDKaggle18Dataset

--- benchmarks/audition/toolbox.py
+++ benchmarks/audition/toolbox.py
@@ -110,27 +110,6 @@ class FSDKaggle18Dataset(Dataset):
     def __len__(self):
         return len(os.listdir(self.audio_dir))
 
-    def plot_waveform(waveform, sample_rate, title="Waveform", xlim=None, ylim=None):
-        waveform = waveform.numpy()
-
-        num_channels, num_frames = waveform.shape
-        time_axis = torch.arange(0, num_frames) / sample_rate
-
-        figure, axes = plt.subplots(num_channels, 1)
-        if num_channels == 1:
-            axes = [axes]
-        for c in range(num_channels):
-            axes[c].plot(time_axis, waveform[c], linewidth=1)
-            axes[c].grid(True)
-            if num_channels > 1:
-                axes[c].set_ylabel(f"Channel {c+1}")
-            if xlim:
-                axes[c].set_xlim(xlim)
-            if ylim:
-                axes[c].set_ylim(ylim)
-        figure.suptitle(title)
-        plt.show(block=False)
-
 
 def load_fsdk18(path_recordings, labels_file, label_arr, feature_type="spectrogram"):
 
