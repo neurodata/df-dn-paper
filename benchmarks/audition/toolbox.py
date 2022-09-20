dcb7675d5990960b1dd4678951b677a4c6f6f76c
PSSF23 <haoyinxu@gmail.com>
2022-09-20 11:31:30 +0000

    ENH save progress for toolbox methods

--- benchmarks/audition/toolbox.py
+++ benchmarks/audition/toolbox.py
@@ -677,7 +677,7 @@ def run_dn_image_es(
     algo = AxSearch(ax_client=ax)
     # Wrap AxSearcher in a concurrently limiter, to ensure that Bayesian optimization receives the
     # data for completed trials before creating more trials
-    algo = tune.suggest.ConcurrencyLimiter(algo, max_concurrent=3)
+    algo = tune.search.ConcurrencyLimiter(algo, max_concurrent=3)
     tune.run(
         train_evaluate,
         num_samples=20,
